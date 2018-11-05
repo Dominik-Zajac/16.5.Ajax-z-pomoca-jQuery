2e2ab40070bbcdba25c2fd82094c86ebe5e07fa8
Dominik Zając <zajac_dominik@onet.pl>
2018-11-05 18:11:19 +0100

    First commit.

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(function() {
+
+    var url = 'http://api.icndb.com/jokes/random';
+    var tweetLink = "https://twitter.com/intent/tweet?text=";
+    var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+    /*---------- Download quote ----------*/
+    function getQuote() {
+        $.getJSON(quoteUrl, createTweet);
+    }
+
+    /*---------- Creat tweet ----------*/
+    function createTweet(input) {
+        var data = input[0];
+        var quoteText = $(data.content).text().trim();
+        var quoteAuthor = data.title;
+
+        if (!quoteAuthor.length) {
+            quoteAuthor = 'Unknown author';
+        }
+
+        var tweetText = 'Quote of the day - ' + quoteText + ' Author: ' + quoteAuthor;
+        if (tweetText.length > 140) {
+            getQuote();
+        } else {
+            var tweet = tweetLink + encodeURIComponent(tweetText);
+
+            $('.quote').text(quoteText);
+            $('.author').text('Author: ' + quoteAuthor);
+            $('.tweet').attr('href', tweet);
+        }
+    }
+
+    /*---------- Generating a quote ----------*/
+    $(document).ready(function() {
+        getQuote();
+        $('.trigger').click(function() {
+            getQuote();
+        })
+    });
+});
