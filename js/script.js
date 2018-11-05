b9666bb561bb3c81806ae32941103a862a6e26e5
Wyrafinowany <41593253+Wyrafinowany@users.noreply.github.com>
2018-11-05 21:15:47 +0100

    Add maximumTwitterPostLength and correct string.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(function() {
 
-	var prefix = "https://cors-anywhere.herokuapp.com/";
-    var tweetLink = "https://twitter.com/intent/tweet?text=";
-    var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+	var prefix = 'https://cors-anywhere.herokuapp.com/';
+    var tweetLink = 'https://twitter.com/intent/tweet?text=';
+    var quoteUrl = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
 
     /*---------- Download quote ----------*/
     function getQuote() {
@@ -21,7 +21,8 @@ $(function() {
         }
 
         var tweetText = 'Quote of the day - ' + quoteText + ' Author: ' + quoteAuthor;
-        if (tweetText.length > 140) {
+	var maximumTwitterPostLength = 140;
+        if (tweetText.length > maximumTwitterPostLength) {
             getQuote();
         } else {
             var tweet = tweetLink + encodeURIComponent(tweetText);
